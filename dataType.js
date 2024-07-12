261f0de65f214f4b99203c372a424df18f3b9905
Keyur Ninama <75513112+Kur09@users.noreply.github.com>
2024-07-12 10:03:32 +0000

    datatypes

---
+++ dataType.js
@@ -0,0 +1,16 @@
+"use strict"; //  treat ll JS code as newer version
+
+let name = "Keyur"
+let age = "23"
+let isLoggedIn = false
+
+// number  => 2 to the power of 53
+//bigInt
+// string 
+// boolean => true/false;
+//NULL => standalone value;
+//undefine => not assign any value
+
+
+console.log(typeof undefined); // undefined
+console.log(typeof NULL);// object;
