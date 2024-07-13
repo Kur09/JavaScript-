76bb76b5384e1a6a4b9e989ee40bafbe018da1d6
Keyur Ninama <75513112+Kur09@users.noreply.github.com>
2024-07-13 19:47:10 +0000

    Date

---
+++ math.js
@@ -0,0 +1,15 @@
+// console.log(Math.abs(-1));
+// console.log(Math.round(4.5));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+
+Math.min(4,32,1,3)
+Math.max(4,2,3,5)
+
+console.log(Math.floor(Math.random()*10) + 1);
+
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max-min+1)) + min )
