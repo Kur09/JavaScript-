1f7e547f190b01e1d68114791db8143711e1d015
Keyur Ninama <75513112+Kur09@users.noreply.github.com>
2024-12-01 05:23:00 +0000

    string methods

---
+++ string.js
@@ -0,0 +1,13 @@
+let username = " keyur ninama"
+
+console.log(username.toUpperCase())
+console.log(username.toLowerCase())
+console.log(username.trim())
+
+console.log(username.includes("k"))
+console.log(username.indexOf('l'))
+
+const a = [1,2,3]
+a[2] = 4
+
+console.log(a)
